89b6c7a26a09bf40dde7e13019765f359097b9af
Wellington Calixto <wac.monti@outlook.com>
2019-08-30 11:28:15 -0300

    listagem de provedres de serviços e criaçao da  model e migrate de agendamento

--- gobarber/src/routes.js
+++ gobarber/src/routes.js
@@ -6,6 +6,7 @@ import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
 import authMiddleware from './app/middlewares/auth';
+import ProviderController from './app/controllers/ProviderController';
 
 const routes = new Router();
 const upload = multer(multerConfig);
@@ -18,4 +19,5 @@ routes.use(authMiddleware);
 routes.put('/users', UserController.update);
 routes.post('/file', upload.single('file'), FileController.store);
 
+routes.get('/providers', ProviderController.index);
 export default routes;
