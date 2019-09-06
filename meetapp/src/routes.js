80bd24de6fd87efd45deadadc4e60c98442e3536
Wellington Calixto <wac.monti@outlook.com>
2019-09-06 11:14:11 -0300

    upload de arquivos e linkagem desse arquivo com um usuario

--- meetapp/src/routes.js
+++ meetapp/src/routes.js
@@ -1,9 +1,12 @@
 import { Router } from 'express';
-
+import multer from 'multer';
+import MulterConfig from './config/multer';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import authMiddlewere from './app/middleware/auth';
+import FileController from './app/controllers/FileController';
 
+const uploads = multer(MulterConfig);
 const routes = new Router();
 
 routes.post('/users', UserController.store);
@@ -12,5 +15,5 @@ routes.post('/login', SessionController.store);
 routes.use(authMiddlewere);
 
 routes.put('/user/update', UserController.update);
-
+routes.post('/file', uploads.single('file'), FileController.store);
 export default routes;
