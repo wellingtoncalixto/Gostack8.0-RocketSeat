f598f2e3ba5d41ccc021bb647df6af1b3074d33f
Wellington Calixto <wac.monti@outlook.com>
2019-09-17 11:39:04 -0300

    feat: adicionando rotas

--- react-modulo5/src/App.js
+++ react-modulo5/src/App.js
@@ -1,13 +1,8 @@
 import React from 'react';
+import Routes from './routes';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>hello</p>
-      </header>
-    </div>
-  );
+  return <Routes />;
 }
 
 export default App;
