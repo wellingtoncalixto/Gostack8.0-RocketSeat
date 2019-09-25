929e0fbca1799364d2269f0086fc7589efd75677
Wellington Calixto <wac.monti@outlook.com>
2019-09-25 15:07:04 -0300

    conf. Estrutura, Eslint, Prettier, Editorconfig

---
+++ modulo6/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import {StyleSheet, Text, View} from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  hello: {
+    fontSize: 30,
+    textAlign: 'center',
+  },
+});
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.hello}>Hello</Text>
+    </View>
+  );
+}
