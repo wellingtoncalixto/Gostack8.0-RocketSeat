5125e6013a40d04d1d659b19fcb66c6031545f92
Wellington Calixto <wac.monti@outlook.com>
2019-09-25 17:08:21 -0300

    feat: routes

--- modulo6/src/index.js
+++ modulo6/src/index.js
@@ -1,24 +1,9 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
 
 import './config/ReactotronConfig';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  hello: {
-    fontSize: 30,
-    textAlign: 'center',
-  },
-});
+import Routes from './routes';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.hello}>Hello</Text>
-    </View>
-  );
+  return <Routes />;
 }
