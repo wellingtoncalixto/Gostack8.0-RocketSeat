3dd7843928b2d66936574be2b6ad47e8eebc2ce6
Wellington Calixto <wac.monti@outlook.com>
2019-10-02 10:19:26 -0300

    feat: adicionando ao carrinho

--- modulo7/src/pages/Home/index.js
+++ modulo7/src/pages/Home/index.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import { MdAddShoppingCart } from 'react-icons/md';
 import { formatPrice } from '../../util/formated';
 import { ProductList } from './styles';
 import api from '../../services/api';
 
-export default class Home extends Component {
+class Home extends Component {
   state = {
     products: [],
   };
@@ -19,6 +20,15 @@ export default class Home extends Component {
     this.setState({ products: data });
   }
 
+  handleAddProduct = product => {
+    const { dispatch } = this.props;
+
+    dispatch({
+      type: 'ADD_TO_CART',
+      product,
+    });
+  };
+
   render() {
     const { products } = this.state;
     return (
@@ -29,7 +39,10 @@ export default class Home extends Component {
             <strong>{product.title}</strong>
             <span>{product.formatPrice}</span>
 
-            <button type="button">
+            <button
+              type="button"
+              onClick={() => this.handleAddProduct(product)}
+            >
               <div>
                 <MdAddShoppingCart size={16} color="#fff" /> 3
               </div>
@@ -42,3 +55,5 @@ export default class Home extends Component {
     );
   }
 }
+
+export default connect()(Home);
