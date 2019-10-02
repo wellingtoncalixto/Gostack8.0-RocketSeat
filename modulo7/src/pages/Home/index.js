4fb9f0eb886412738b34e4fe9a337f36e647e7d6
Wellington Calixto <wac.monti@outlook.com>
2019-10-02 09:42:30 -0300

    feat: trazendo dados da api

--- modulo7/src/pages/Home/index.js
+++ modulo7/src/pages/Home/index.js
@@ -1,90 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { MdAddShoppingCart } from 'react-icons/md';
+import { formatPrice } from '../../util/formated';
 import { ProductList } from './styles';
+import api from '../../services/api';
 
-export default function Home() {
-  return (
-    <ProductList>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-vibe-crew-masculino/26/411-3605-026/411-3605-026_zoom1.jpg"
-          alt=""
-        />
-        <strong>Tenis</strong>
-        <span>R$ 120,00</span>
+export default class Home extends Component {
+  state = {
+    products: [],
+  };
 
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#fff" /> 3
-          </div>
+  async componentDidMount() {
+    const response = await api.get('/products');
 
-          <span>Adicionar ao carrino</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-vibe-crew-masculino/26/411-3605-026/411-3605-026_zoom1.jpg"
-          alt=""
-        />
-        <strong>Tenis</strong>
-        <span>R$ 120,00</span>
+    const data = response.data.map(product => ({
+      ...product,
+      formatPrice: formatPrice(product.price),
+    }));
+    this.setState({ products: data });
+  }
 
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#fff" /> 3
-          </div>
+  render() {
+    const { products } = this.state;
+    return (
+      <ProductList>
+        {products.map(product => (
+          <li key={product.id}>
+            <img src={product.image} alt={product.title} />
+            <strong>{product.title}</strong>
+            <span>{product.formatPrice}</span>
 
-          <span>Adicionar ao carrino</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-vibe-crew-masculino/26/411-3605-026/411-3605-026_zoom1.jpg"
-          alt=""
-        />
-        <strong>Tenis</strong>
-        <span>R$ 120,00</span>
+            <button type="button">
+              <div>
+                <MdAddShoppingCart size={16} color="#fff" /> 3
+              </div>
 
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#fff" /> 3
-          </div>
-
-          <span>Adicionar ao carrino</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-vibe-crew-masculino/26/411-3605-026/411-3605-026_zoom1.jpg"
-          alt=""
-        />
-        <strong>Tenis</strong>
-        <span>R$ 120,00</span>
-
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#fff" /> 3
-          </div>
-
-          <span>Adicionar ao carrino</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-vibe-crew-masculino/26/411-3605-026/411-3605-026_zoom1.jpg"
-          alt=""
-        />
-        <strong>Tenis</strong>
-        <span>R$ 120,00</span>
-
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#fff" /> 3
-          </div>
-
-          <span>Adicionar ao carrino</span>
-        </button>
-      </li>
-    </ProductList>
-  );
+              <span>Adicionar ao carrino</span>
+            </button>
+          </li>
+        ))}
+      </ProductList>
+    );
+  }
 }
