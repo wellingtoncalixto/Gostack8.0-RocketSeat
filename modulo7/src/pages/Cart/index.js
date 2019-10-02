a9c24cb8ac59ec922f1d40fd269bfc070d32983c
Wellington Calixto <wac.monti@outlook.com>
2019-10-02 15:01:35 -0300

    feat: eclur produto do carrinho

--- modulo7/src/pages/Cart/index.js
+++ modulo7/src/pages/Cart/index.js
@@ -7,7 +7,7 @@ import {
 } from 'react-icons/md';
 import { Container, ProductTable, Total } from './styles';
 
-function Cart({ cart }) {
+function Cart({ cart, dispatch }) {
   return (
     <Container>
       <ProductTable>
@@ -45,7 +45,12 @@ function Cart({ cart }) {
                 <strong>R$100,00</strong>
               </td>
               <td>
-                <button type="button">
+                <button
+                  type="button"
+                  onClick={() =>
+                    dispatch({ type: 'REMOVE_FROM_CART', id: product.id })
+                  }
+                >
                   <MdDelete size={20} color="#191920" />
                 </button>
               </td>
