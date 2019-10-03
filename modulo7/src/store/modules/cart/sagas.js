253ed41de4826e6edfe5ed1d7d16c662d9cbe47e
Wellington Calixto <wac.monti@outlook.com>
2019-10-03 14:28:30 -0300

    Reactotron + Saga; Separando Actions; Verificando estoque; React Toastify

--- modulo7/src/store/modules/cart/sagas.js
+++ modulo7/src/store/modules/cart/sagas.js
@@ -1,13 +1,38 @@
-import { call, put, all, takeLatest } from 'redux-saga/effects';
+import { call, select, put, all, takeLatest } from 'redux-saga/effects';
+import { toast } from 'react-toastify';
 
+import { formatPrice } from '../../../util/formated';
 import api from '../../../services/api';
 
-import { addToCartSuccess } from './actions';
+import { addToCartSuccess, updateAmount } from './actions';
 
 function* addToCart({ id }) {
-  const response = yield call(api.get, `/products/${id}`);
+  const productExist = yield select(state => state.cart.find(p => p.id === id));
 
-  yield put(addToCartSuccess(response.data));
+  const stock = yield call(api.get, `/stock/${id}`);
+
+  const stockAmount = stock.data.amount;
+  const currentAmount = productExist ? productExist.amount : 0;
+
+  const amount = currentAmount + 1;
+
+  if (amount > stockAmount) {
+    toast.error('quantidade solicitada fora de estoque');
+    return;
+  }
+
+  if (productExist) {
+    yield put(updateAmount(id, amount));
+  } else {
+    const response = yield call(api.get, `/products/${id}`);
+
+    const data = {
+      ...response.data,
+      amount: 1,
+      formatPrice: formatPrice(response.data.price),
+    };
+    yield put(addToCartSuccess(data));
+  }
 }
 
 export default all([takeLatest('@cart/ADD_REQUEST', addToCart)]);
