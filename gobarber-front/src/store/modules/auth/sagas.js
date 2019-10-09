7b5bdd8409cb60f86a1462416960ef0eb90b7c38
Wellington Calixto <wac.monti@outlook.com>
2019-10-09 15:32:37 -0300

    exibindo mensagem de error com Toastify

--- gobarber-front/src/store/modules/auth/sagas.js
+++ gobarber-front/src/store/modules/auth/sagas.js
@@ -1,5 +1,5 @@
 import { takeLatest, call, put, all } from 'redux-saga/effects';
-
+import { toast } from 'react-toastify';
 import api from '~/services/api';
 import history from '~/services/history';
 import { signInSuccess, signFailure } from './actions';
@@ -16,7 +16,7 @@ export function* signIn({ payload }) {
     const { token, user } = response.data;
 
     if (!user.provider) {
-      console.tron.error('usuario não é um prestador de serviços');
+      toast.error('usuario não é um prestador de serviços');
       return;
     }
 
@@ -24,6 +24,7 @@ export function* signIn({ payload }) {
 
     history.push('/dashboard');
   } catch (err) {
+    toast.error('Falha na autenticação, verifique os seus dados');
     yield put(signFailure());
   }
 }
