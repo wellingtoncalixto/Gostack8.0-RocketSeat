74b922fe0aa89df507f49b3e99f675210ec478ab
Wellington Calixto <wac.monti@outlook.com>
2019-10-08 22:04:41 -0300

    estilizanção da autenticação

--- gobarber-front/src/pages/SingUp/index.js
+++ gobarber-front/src/pages/SingUp/index.js
@@ -1,7 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-// import { Container } from './styles';
+import logo from '~/assets/logo.svg';
 
 export default function SingUp() {
-  return <div />;
+  return (
+    <>
+      <img src={logo} alt="GoBarber" />
+
+      <form action="">
+        <input placeholder="Nome Completo" />
+        <input type="email" placeholder="Seu Email" />
+        <input type="password" placeholder="Sua senha secreta" />
+
+        <button type="submit">Criar Conta</button>
+        <Link to="/">Já tenho Login</Link>
+      </form>
+    </>
+  );
 }
