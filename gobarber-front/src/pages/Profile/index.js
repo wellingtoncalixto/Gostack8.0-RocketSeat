8f76d153f45fa8a1dfc1016ced0d6bfb5e4b4dbe
Wellington Calixto <wac.monti@outlook.com>
2019-10-10 16:46:25 -0300

    atualizando foto de perfil

--- gobarber-front/src/pages/Profile/index.js
+++ gobarber-front/src/pages/Profile/index.js
@@ -3,6 +3,7 @@ import { Form, Input } from '@rocketseat/unform';
 import { useDispatch, useSelector } from 'react-redux';
 import { Container } from './styles';
 import { updateProfileRequest } from '~/store/modules/user/actions';
+import AvatarInput from './AvatarInput';
 
 export default function Profile() {
   const dispath = useDispatch();
@@ -14,6 +15,7 @@ export default function Profile() {
   return (
     <Container>
       <Form initialData={profile} onSubmit={handleSubmit}>
+        <AvatarInput name="avatar_id" />
         <Input name="name" placeholder="Nome Completo" />
         <Input name="email" type="email" placeholder="Seu email" />
 
--- gobarber-front/src/store/modules/user/sagas.js
+++ gobarber-front/src/store/modules/user/sagas.js
@@ -5,9 +5,14 @@ import { updateProfileSuccess, updateProfileFailute } from './actions';
 
 export function* updateProfile({ payload }) {
   try {
-    const { name, email, ...rest } = payload.data;
+    const { name, email, avatar_id, ...rest } = payload.data;
 
-    const profile = { name, email, ...(rest.oldPassword ? rest : {}) };
+    const profile = {
+      name,
+      email,
+      avatar_id,
+      ...(rest.oldPassword ? rest : {}),
+    };
 
     const response = yield call(api.put, 'users', profile);
 
