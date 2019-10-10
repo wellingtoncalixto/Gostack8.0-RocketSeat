657cc2a859b55a1b66a15e79823659f3d666a714
Wellington Calixto <wac.monti@outlook.com>
2019-10-10 14:08:45 -0300

    atualizando dados do perfil do usuario

--- gobarber-front/src/pages/Profile/index.js
+++ gobarber-front/src/pages/Profile/index.js
@@ -1,7 +1,40 @@
 import React from 'react';
-
-// import { Container } from './styles';
+import { Form, Input } from '@rocketseat/unform';
+import { useDispatch, useSelector } from 'react-redux';
+import { Container } from './styles';
+import { updateProfileRequest } from '~/store/modules/user/actions';
 
 export default function Profile() {
-  return <div />;
+  const dispath = useDispatch();
+  const profile = useSelector(state => state.user.profile);
+
+  function handleSubmit(data) {
+    dispath(updateProfileRequest(data));
+  }
+  return (
+    <Container>
+      <Form initialData={profile} onSubmit={handleSubmit}>
+        <Input name="name" placeholder="Nome Completo" />
+        <Input name="email" type="email" placeholder="Seu email" />
+
+        <hr />
+
+        <Input
+          name="oldPassword"
+          type="password"
+          placeholder="Sua senha atual"
+        />
+        <Input name="password" type="password" placeholder="Nova Senha" />
+        <Input
+          name="confirmPassword"
+          type="password"
+          placeholder="Confirmação de senha"
+        />
+
+        <button type="submit">Atualizar Perfil</button>
+      </Form>
+
+      <button type="button">Sair do GoBarber</button>
+    </Container>
+  );
 }
--- gobarber-front/src/store/modules/user/sagas.js
+++ gobarber-front/src/store/modules/user/sagas.js
@@ -1,3 +1,22 @@
-import { all } from 'redux-saga/effects';
+import { all, takeLatest, call, put } from 'redux-saga/effects';
+import { toast } from 'react-toastify';
+import api from '~/services/api';
+import { updateProfileSuccess, updateProfileFailute } from './actions';
 
-export default all([]);
+export function* updateProfile({ payload }) {
+  try {
+    const { name, email, ...rest } = payload.data;
+
+    const profile = { name, email, ...(rest.oldPassword ? rest : {}) };
+
+    const response = yield call(api.put, 'users', profile);
+
+    toast.success('Perfil atualizado com sucesso!');
+
+    yield put(updateProfileSuccess(response.data));
+  } catch (err) {
+    toast.error('Erro ao atualizar perfil, confira seuas dados');
+    yield put(updateProfileFailute());
+  }
+}
+export default all([takeLatest('@user/UPDATE_PROFILE_REQUEST', updateProfile)]);
