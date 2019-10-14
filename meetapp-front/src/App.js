4efccd5c9859583d2b1cc17b7d9732681cc7b1cc
Wellington Calixto <wac.monti@outlook.com>
2019-10-14 16:11:02 -0300

    feat: persistindo dados do usuario

--- meetapp-front/src/App.js
+++ meetapp-front/src/App.js
@@ -1,23 +1,26 @@
 import React from 'react';
-
+import { PersistGate } from 'redux-persist/integration/react';
 import { Provider } from 'react-redux';
 import { Router } from 'react-router-dom';
 
 import './config/reactotronConfig';
-import Routes from './routes';
-import GlobalStyle from './styles/global';
 
+import Routes from './routes/index';
 import history from './service/history';
 
-import store from './store';
+import { store, persistor } from './store';
+
+import GlobalStyle from './styles/global';
 
 function App() {
   return (
     <Provider store={store}>
-      <Router history={history}>
-        <Routes />
-        <GlobalStyle />
-      </Router>
+      <PersistGate persistor={persistor}>
+        <Router history={history}>
+          <Routes />
+          <GlobalStyle />
+        </Router>
+      </PersistGate>
     </Provider>
   );
 }
