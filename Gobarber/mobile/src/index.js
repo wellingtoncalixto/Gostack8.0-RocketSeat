153312a788c0bd00499054f8d9f14b26758e48db
Wellington Calixto <wac.monti@outlook.com>
2019-10-17 16:48:39 -0300

    estilizando statusBar

--- Gobarber/mobile/src/index.js
+++ Gobarber/mobile/src/index.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import {StatusBar} from 'react-native';
+
 import Background from '~/components/Background';
 import Routes from './routes';
 // import { Container } from './styles';
@@ -6,6 +8,7 @@ import Routes from './routes';
 export default function App() {
   return (
     <Background>
+      <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
       <Routes />
     </Background>
   );
