fb0efd42b0dbe1365312a93fc935e97f37809233
Wellington Calixto <wac.monti@outlook.com>
2019-10-17 16:45:28 -0300

    estilizando paginas de SignIn e SignUp

--- Gobarber/mobile/src/pages/SignIn/index.js
+++ Gobarber/mobile/src/pages/SignIn/index.js
@@ -1,8 +1,40 @@
 import React from 'react';
-import {Text} from 'react-native';
+import {Image} from 'react-native';
 
-// import { Container } from './styles';
+import logo from '~/assets/logo.png';
 
-export default function SignIn() {
-  return <Text>ola</Text>;
+import {
+  Container,
+  Form,
+  FormInput,
+  SubmitiButton,
+  SignLink,
+  SignLinkText,
+} from './styles';
+
+export default function SignIn({navigation}) {
+  return (
+    <Container>
+      <Image source={logo} />
+
+      <Form>
+        <FormInput
+          icon="mail-outline"
+          keyboardType="email-address"
+          autoCorrect={false}
+          autoCapitalize="none"
+          placeholder="digite seu e-mail"
+        />
+        <FormInput
+          icon="lock-outline"
+          secureTextEntry
+          placeholder="Sua Senha"
+        />
+        <SubmitiButton onPress={() => {}}>Acessar</SubmitiButton>
+      </Form>
+      <SignLink onPress={() => navigation.navigate('SignUp')}>
+        <SignLinkText>Criar conta gratuita</SignLinkText>
+      </SignLink>
+    </Container>
+  );
 }
--- Gobarber/mobile/src/pages/SignUp/index.js
+++ Gobarber/mobile/src/pages/SignUp/index.js
@@ -1,8 +1,46 @@
 import React from 'react';
-import {View} from 'react-native';
+import {Image} from 'react-native';
 
-// import { Container } from './styles';
+import logo from '~/assets/logo.png';
 
-export default function SignUp() {
-  return <View />;
+import {
+  Container,
+  Form,
+  FormInput,
+  SubmitiButton,
+  SignLink,
+  SignLinkText,
+} from './styles';
+
+export default function SignUp({navigation}) {
+  return (
+    <Container>
+      <Image source={logo} />
+
+      <Form>
+        <FormInput
+          icon="person-outline"
+          autoCorrect={false}
+          autoCapitalize="none"
+          placeholder="digite seu nome completo"
+        />
+        <FormInput
+          icon="mail-outline"
+          keyboardType="email-address"
+          autoCorrect={false}
+          autoCapitalize="none"
+          placeholder="digite seu e-mail"
+        />
+        <FormInput
+          icon="lock-outline"
+          secureTextEntry
+          placeholder="Sua Senha"
+        />
+        <SubmitiButton onPress={() => {}}>Cadastrar</SubmitiButton>
+      </Form>
+      <SignLink onPress={() => navigation.navigate('SignIn')}>
+        <SignLinkText>Já tenho uma conta</SignLinkText>
+      </SignLink>
+    </Container>
+  );
 }
