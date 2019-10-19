14efce4b30033acac50a4e229efb7f4212ae71fb
Wellington Calixto <wac.monti@outlook.com>
2019-10-19 16:42:59 -0300

    fazendo a estilizanção da lista de agendamentos

--- Gobarber/mobile/src/pages/Dashboard/index.js
+++ Gobarber/mobile/src/pages/Dashboard/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
-import {View} from 'react-native';
+import Appointments from '~/components/Appointments';
 
-// import { Container } from './styles';
+import {Container, Title, List} from './styles';
+
+const data = [1, 2, 3, 4, 5];
 
 export default function Dashboard() {
-  return <View />;
+  return (
+    <Container>
+      <Title>Agendamentos</Title>
+      <List
+        data={data}
+        keyExtractor={item => String(item)}
+        renderItem={({item}) => <Appointments data={item} />}
+      />
+    </Container>
+  );
 }
