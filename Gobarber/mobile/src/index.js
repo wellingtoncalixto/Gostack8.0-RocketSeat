3aaedb2a8374eb9a8f62889820b768839a343b10
Wellington Calixto <wac.monti@outlook.com>
2019-10-19 15:20:51 -0300

    Rota Inicial

--- Gobarber/mobile/src/index.js
+++ Gobarber/mobile/src/index.js
@@ -6,17 +6,18 @@ import {StatusBar} from 'react-native';
 import '~/config/ReactotronConfig';
 
 import Background from '~/components/Background';
-import Routes from './routes';
+
+import App from './App';
 
 import {store, persistor} from './store';
 
-export default function App() {
+export default function Index() {
   return (
     <Provider store={store}>
       <PersistGate persistor={persistor}>
         <Background>
           <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
-          <Routes />
+          <App />
         </Background>
       </PersistGate>
     </Provider>
