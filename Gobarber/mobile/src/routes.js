6991feb209d70e5629dde7ff6a67a270db4d08cc
Wellington Calixto <wac.monti@outlook.com>
2019-10-19 16:10:33 -0300

    Estilizando rotas

--- Gobarber/mobile/src/routes.js
+++ Gobarber/mobile/src/routes.js
@@ -1,10 +1,14 @@
+import React from 'react';
 import {createAppContainer, createSwitchNavigator} from 'react-navigation';
 import {createBottomTabNavigator} from 'react-navigation-tabs';
+
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 import SignIn from '~/pages/SignIn';
 import SignUp from '~/pages/SignUp';
 
 import Dashboard from '~/pages/Dashboard';
+import Profile from '~/pages/Profile';
 
 export default (isSigned = false) =>
   createAppContainer(
@@ -14,9 +18,36 @@ export default (isSigned = false) =>
           SignIn,
           SignUp,
         }),
-        App: createBottomTabNavigator({
-          Dashboard,
-        }),
+        App: createBottomTabNavigator(
+          {
+            Dashboard: {
+              screen: Dashboard,
+              navigationOptions: {
+                tabBarLabel: 'Agendamentos',
+                tabBarIcon: ({tintColor}) => (
+                  <Icon name="event" size={20} color={tintColor} />
+                ),
+              },
+            },
+            Profile: {
+              screen: Profile,
+              navigationOptions: {
+                tabBarLabel: 'Perfil',
+                tabBarIcon: ({tintColor}) => (
+                  <Icon name="person" size={20} color={tintColor} />
+                ),
+              },
+            },
+          },
+          {
+            tabBarOptions: {
+              keyboardHidesTabBar: true,
+              activeTintColor: '#fff',
+              inactiveTintColor: 'rgba(255,255,255,0.6)',
+              style: {backgroundColor: '#8d41a8'},
+            },
+          }
+        ),
       },
       {
         initialRouteName: isSigned ? 'App' : 'Sign',
