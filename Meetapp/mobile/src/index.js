3d5f812b44b5745aef2fb222d74ca44ae454118b
Wellington Calixto <wac.monti@outlook.com>
2019-10-21 15:53:01 -0300

    config. rotas

--- Meetapp/mobile/src/index.js
+++ Meetapp/mobile/src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import {View} from 'react-native';
+
 import '~/config/ReactotronConfig';
 
+import Routes from './routes';
 // import { Container } from './styles';
 
 export default function Ap() {
-  return <View />;
+  return <Routes />;
 }
