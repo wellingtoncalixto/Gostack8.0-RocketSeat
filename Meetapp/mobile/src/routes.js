5898895bc4ba2569e1a4860fa1a99529c08ab088
Wellington Calixto <wac.monti@outlook.com>
2019-10-21 16:37:08 -0300

    config. rotas de autenticação

--- Meetapp/mobile/src/routes.js
+++ Meetapp/mobile/src/routes.js
@@ -1,13 +1,25 @@
 import {createAppContainer, createSwitchNavigator} from 'react-navigation';
 
+import {createBottomTabNavigator} from 'react-navigation-tabs';
 import SignIn from '~/pages/SignIn';
 import SignUp from '~/pages/SignUp';
 
-const Routes = createAppContainer(
-  createSwitchNavigator({
-    SignIn,
-    SignUp,
-  })
-);
+import Dashboard from '~/pages/Dashboard';
 
-export default Routes;
+export default (isSigned = false) =>
+  createAppContainer(
+    createSwitchNavigator(
+      {
+        Sign: createSwitchNavigator({
+          SignIn,
+          SignUp,
+        }),
+        App: createBottomTabNavigator({
+          Dashboard,
+        }),
+      },
+      {
+        initialRouteName: isSigned ? 'App' : 'Sign',
+      }
+    )
+  );
